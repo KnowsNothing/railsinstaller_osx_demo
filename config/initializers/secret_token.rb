d7c70e492686b41ee56a0006ca0ecf100b3c137f
Matt Edwards <matt@registrylove.com>
2012-07-03 14:03:49 -0700

    added welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = '91f554f4936916a48708e4c53f9927335af3992902a917e9da074b90f65375f1e6b9b7b32b411aed976094b28409efcd4ca5a219a1b5ca7acfdf60785dbc507d'
